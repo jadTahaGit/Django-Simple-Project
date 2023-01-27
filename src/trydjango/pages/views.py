eb5a8f68ae5089516b556c4a19f49cad4fe2b925
Jad Taha <jadtaha.de@gmail.com>
2023-01-27 10:54:42 +0100

    forLoop  in a Template --version01.60.46

--- src/trydjango/pages/views.py
+++ src/trydjango/pages/views.py
@@ -6,7 +6,13 @@ def home_view(*args, **kwargs):
     return HttpResponse("<h1> Hello World </h1>")
 
 def home_view_02(request,*args, **kwargs):
-    return render(request, "home.html", {})
+    my_context = {
+        "myText": "This is about Us",
+        "myNumber": 2023,
+        "myList" : {1,2,3,4,5,6,7,8}
+    }
+    
+    return render(request, "home.html", my_context)
 
 def about_view(request,*args, **kwargs):
     my_context = {
