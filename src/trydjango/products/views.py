f0c92957e9d31eba5ff2a727f4012e190f7abf49
Jad Taha <jadtaha.de@gmail.com>
2023-01-27 11:39:13 +0100

    dataRendered | removed the Destruction of Product Object in Views --version01.60.57

--- src/trydjango/products/views.py
+++ src/trydjango/products/views.py
@@ -4,9 +4,6 @@ from .models import Product
 def product_detail_view(request):
     obj = Product.objects.get(id=1)
     context = {
-        'title': obj.title,
-        'description': obj.description,
-        "price": obj.price
-
+        'object': obj
     }
     return render(request, "product/detail.html", context)
